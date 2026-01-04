eac9b6052f177f4e7dd6d78d30b892e889ceb513
Andy Park <park.andy000@gmail.com>
2026-01-04 12:34:59 -0500

    i forgot changes

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -4,6 +4,8 @@ import SBEngaged from '../../../assets/SBEngaged.png'
 import SBU_Logo_Shield from '../../../assets/SBU_Logo_Shield.png'
 import SBU_Logo_ShieldText from '../../../assets/SBU_Logo_ShieldText.png'
 import Menu from '../../../assets/Menu.svg'
+import Inbox from '../../../assets/Inbox.svg'
+import {Calendar, SBEngaged, Inbox} from '../../../assets'
 import { Router, Routes, Link } from 'react-router-dom'
 
 
@@ -13,15 +15,27 @@ const Navbar = () => {
 
   }
   return (
-    <div>
-      <nav class="navbar">
-        <button class="nav-button">
-          <img src={Menu}/>
-        </button>
-        <a href="http://localhost:5173/">
-          <img src={SBEngaged} />
-        </a>
-      </nav>
+    <div class="topbarcontainer">
+    <ul class="topleft">
+        <li>
+          <a href="#menu"> 
+            <img src="./assets/Menu.png" alt="<menu>" style="width:48px; height:36px;" />
+          </ a>
+        </li>
+        <li>
+          <a href="http://localhost:xxxx"> 
+            <img src="./assets/Logo.png" alt="<logo>" style="width:286px; height:69px;"/>
+          </a>
+        </li>
+    </ul>
+
+
+    <ul class="topright">
+        <li><Link to='/inbox'> <img src={Inbox} alt="<inbox>" style="width:34px; height:28px;"> </Link>
+        <li><a href="#settings"> <img src={Settings} alt="<settings>" style="width:37px; height:37px;"> </a>
+        <li><a href="#profile"> <img src="./assets/Profile.png" alt="<profile>" style="width:61px; height:61px;"> </a>
+    </ul>
+    <img src="./assets/Rectangle.png" alt="<bg>" style="width:130; height:46; position: fixed; z-index: 2; top: 10px; right: 39px;">
     </div>
   )
 }
