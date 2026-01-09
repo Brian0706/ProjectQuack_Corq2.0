8492b88f39a493e4a7b232e5886caf32e6aec7fb
Andy Park <park.andy000@gmail.com>
2026-01-09 14:10:21 -0500

    updated changes so that ug can merge

--- ProjectQuack/src/App.jsx
+++ ProjectQuack/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import {ResquestTester} from './testingStuff/requestTester'
+import RequestTester from './testingStuff/requestTester'
 import './App.css'
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <div>
-        <
+        <RequestTester/>
       </div>
     </>
   )
